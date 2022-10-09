a806b7f039b0feec050c8f66a2a92f2990c4fe1d
Jozef Mlich <jmlich83@gmail.com>
2022-10-09 11:32:06 +0000

    lupdate

--- translations/accounts.ts
+++ translations/accounts.ts
@@ -2,9 +2,22 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>AccountsSettingsPlugin</name>
+    <message>
+        <location filename="../accountssettingsplugin.h" line="33"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../accountssettingsplugin.h" line="34"/>
+        <source>Accounts settings plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>CreateAccountSheet</name>
     <message>
-        <location filename="../qml/plugins/accounts/CreateAccountSheet.qml" line="42"/>
+        <location filename="../CreateAccountSheet.qml" line="46"/>
         <source>Accounts</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12,138 +25,151 @@
 <context>
     <name>accounts</name>
     <message>
-        <location filename="../qml/plugins/accounts/accounts.qml" line="42"/>
+        <location filename="../accounts.qml" line="45"/>
         <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../accounts.qml" line="74"/>
+        <source>No accounts</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>email</name>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="44"/>
+        <location filename="../providers/email/qml/email.qml" line="44"/>
         <source>Email</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="139"/>
+        <location filename="../providers/email/qml/email.qml" line="139"/>
         <source>Incoming server type</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="143"/>
+        <location filename="../providers/email/qml/email.qml" line="143"/>
         <source>IMAP4</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="144"/>
+        <location filename="../providers/email/qml/email.qml" line="144"/>
         <source>POP3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="163"/>
+        <location filename="../providers/email/qml/email.qml" line="163"/>
         <source>Incoming server username</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="176"/>
+        <location filename="../providers/email/qml/email.qml" line="176"/>
         <source>Incoming server password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="188"/>
+        <location filename="../providers/email/qml/email.qml" line="188"/>
         <source>Incoming server address</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="193"/>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="258"/>
+        <location filename="../providers/email/qml/email.qml" line="193"/>
+        <location filename="../providers/email/qml/email.qml" line="258"/>
         <source>Secure connection</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="197"/>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="262"/>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="293"/>
+        <location filename="../providers/email/qml/email.qml" line="197"/>
+        <location filename="../providers/email/qml/email.qml" line="262"/>
+        <location filename="../providers/email/qml/email.qml" line="293"/>
         <source>None</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="198"/>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="263"/>
+        <location filename="../providers/email/qml/email.qml" line="198"/>
+        <location filename="../providers/email/qml/email.qml" line="263"/>
         <source>SSL</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="199"/>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="264"/>
+        <location filename="../providers/email/qml/email.qml" line="199"/>
+        <location filename="../providers/email/qml/email.qml" line="264"/>
         <source>TLS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="219"/>
+        <location filename="../providers/email/qml/email.qml" line="219"/>
         <source>Incoming server port</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="226"/>
+        <location filename="../providers/email/qml/email.qml" line="226"/>
         <source>Outgoing server username</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="242"/>
+        <location filename="../providers/email/qml/email.qml" line="242"/>
         <source>Outgoing server password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="253"/>
+        <location filename="../providers/email/qml/email.qml" line="253"/>
         <source>Outgoing server address</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="284"/>
+        <location filename="../providers/email/qml/email.qml" line="284"/>
         <source>Outgoing server port</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="290"/>
+        <location filename="../providers/email/qml/email.qml" line="290"/>
         <source>Authentication</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="294"/>
+        <location filename="../providers/email/qml/email.qml" line="294"/>
         <source>Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="295"/>
+        <location filename="../providers/email/qml/email.qml" line="295"/>
         <source>Encrypted Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/email.qml" line="312"/>
+        <location filename="../providers/email/qml/email.qml" line="312"/>
         <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>google</name>
+    <message>
+        <location filename="../providers/google/qml/google.qml" line="45"/>
+        <source>Google</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>jabber</name>
     <message>
-        <location filename="../qml/plugins/accounts/providers/jabber.qml" line="43"/>
+        <location filename="../providers/jabber/qml/jabber.qml" line="43"/>
         <source>Jabber</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/jabber.qml" line="140"/>
+        <location filename="../providers/jabber/qml/jabber.qml" line="136"/>
         <source>Username</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/jabber.qml" line="148"/>
+        <location filename="../providers/jabber/qml/jabber.qml" line="144"/>
         <source>Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/plugins/accounts/providers/jabber.qml" line="157"/>
+        <location filename="../providers/jabber/qml/jabber.qml" line="153"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
